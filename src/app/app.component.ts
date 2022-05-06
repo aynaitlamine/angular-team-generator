4884f5e7257b88eb22fb330efcfc077ae7d035ce
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2022-05-06 10:18:23 +0000

    Adding logic and separate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-team-generator';
-  newMemberName: string = '';
+  newMember = '';
   members: string[] = [];
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
+  errorMessage = '';
 
   onInput(member: string) {
-    if (member != '') {
-      this.newMemberName = member;
-    }
+    this.newMember = member;
   }
 
-  addMember() {
-    this.members.push(this.newMemberName);
-    console.log(this.members);
+  onClick() {
+    if (!this.newMember.length) {
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMember);
+    this.newMember = '';
+  }
+
+  onTeamSizeInput(value: string) {
+    this.numberOfTeams = Number(value);
+  }
+
+  generateTeams() {
+    this.teams = [];
+    const allMembers = [...this.members];
+
+    if (this.members.length < this.numberOfTeams) {
+      this.errorMessage = 'Not enough members';
+      return;
+    }
+
+    this.errorMessage = '';
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = '';
   }
 }
