0e1a585b4b73d9ad5ac2f56b8fecdccf073dda5e
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2022-05-06 08:51:09 +0000

    Adding simple ui and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-team-generator';
+  newMemberName: string = '';
+  members: string[] = [];
+
+  onInput(member: string) {
+    if (member != '') {
+      this.newMemberName = member;
+    }
+  }
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
 }
